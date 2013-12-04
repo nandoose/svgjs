82f87bc24befed387b6e98e22c6ee7e41bd7b1a2
nandoose <to.nandoose@gmail.com>
2013-12-04 17:51:00 -0600

    Flag added

--- svg.ashoka.js
+++ svg.ashoka.js
@@ -1,33 +1,39 @@
 
 SVG.Ashoka = function(size, options) {
 
-  this.constructor.call(this, SVG.create('svg'))
-
+  this.constructor.call(this, SVG.create('svg'));
 
   this.viewbox(0, 0, 100, 100);
 
   this.size(size, size);
 
-  this.chakra = this.group()
+  var emblem  = this.set();
+  var wheel = this.group()
                   .add(this.circle(50).fill("#008").center(50,50))
                   .add(this.circle(45).fill("#fff").center(50,50))
-                  .add(this.circle(10).fill("#008").center(50,50))
+                  .add(this.circle(10).fill("#008").center(50,50));
 
+  emblem.add(wheel)
 
+ var spokes = this.group()
 
 for (var i = 0; i < 24 ; i++){
-    this.group()
-      .add(this.polygon('0,0 1.0,12 0,24 -1.0,12').fill("#008"))
-      .add(this.circle(3).fill("#008").move(-4.5,21))
+    var spoke = this.group()
+      .add(this.polygon('0,0 1.0,11 0,23.5 -1.0,11').fill("#008"))
+      .add(this.circle(2.3).fill("#008").move(-4.3,21.3))
       .rotate(i*15)
-      .move(50,50)
+      .move(50,50);
+      spokes.add(spoke)
 }
 
+emblem.add(spokes)
+emblem.animate(3000).rotate(100)
+
 
 
 }
 
-SVG.Ashoka.prototype = new SVG.Container
+SVG.Ashoka.prototype = new SVG.Container ;
 
 
 
@@ -38,4 +44,4 @@ SVG.extend(SVG.Container, {
     return this.put(new SVG.Ashoka(size))
   }
   
-})
+});
